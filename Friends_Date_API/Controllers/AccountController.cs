42bab5c5a0cf3531c3fcc8be2b0481565958aabf
MH Dip <mhdip05@gmail.com>
2022-03-12 23:25:45 +0600

    Basic Authentication, Register,  and login

---
+++ Friends_Date_API/Controllers/AccountController.cs
@@ -0,0 +1,75 @@
+﻿using Friends_Date_API.Data;
+using Friends_Date_API.DTO;
+using Friends_Date_API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Friends_Date_API.Controllers
+{
+    public class AccountController : BaseAPIController
+    {
+        private readonly DataContext _context;
+
+        public AccountController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost("Register")]
+        public async Task<ActionResult<AppUser>> Register(RegisterDto registerDto)
+        {
+            //check user name is exist or not
+            if (await UserExists(registerDto.UserName))
+                return BadRequest("Username is taken");
+
+            //use security class to make password hast
+            using var hmac = new HMACSHA512();
+
+            //set value to Users Object
+            var user = new AppUser
+            {
+                UserName = registerDto.UserName.ToLower(),
+                PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password)),
+                PasswordSalt = hmac.Key
+            };
+
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+
+            return user;
+        }
+
+        private async Task<bool> UserExists(string username)
+        {
+            return await _context.Users.AnyAsync(x => x.UserName == username.ToLower());
+        }
+
+        [HttpPost("Login")]
+        public async Task<ActionResult<AppUser>> Login(LoginDto loginDto)
+        {
+            //fetch and check Data from user
+            var user = await _context.Users.SingleOrDefaultAsync(x => x.UserName == loginDto.UserName.ToLower());
+            if (user == null)
+                return BadRequest("Invalid Username");
+
+            //if the user name match then we convert selected user passwordSalt which give us password hash value in byte string
+            using var hmac = new HMACSHA512(user.PasswordSalt);
+
+            // we convert user entered password to byte string which
+            var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
+
+            // check weather databash hash and user entered password hash matched
+            for (int i = 0; i < computedHash.Length; i++)
+            {
+                if (computedHash[i] != user.PasswordHash[i])
+                    return Unauthorized("Invalid Password");
+            }
+
+            return user;
+        }
+    }
+}
---
+++ Friends_Date_API/Controllers/BaseAPIController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Friends_Date_API.Controllers
+{
+    [ApiController]
+    [Route("api/[Controller]")]
+    public class BaseAPIController : ControllerBase
+    {
+       
+    }
+}
