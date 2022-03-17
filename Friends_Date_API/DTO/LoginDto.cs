f3fcadc903083a8021b7eb41fe9ef6bde8a8c65b
MH Dip <mhdip05@gmail.com>
2022-03-17 17:21:33 +0600

    Add Validation to LoginDto

--- Friends_Date_API/DTO/LoginDto.cs
+++ Friends_Date_API/DTO/LoginDto.cs
@@ -1,8 +1,13 @@
-﻿namespace Friends_Date_API.DTO
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Friends_Date_API.DTO
 {
     public class LoginDto
     {
+        [Required]
         public string UserName { get; set; }
+
+        [Required]
         public string Password { get; set; }
     }
 }
